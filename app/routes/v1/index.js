6ab0479ef6b46867974fda26e708c78a046e4348
takayuki.kayawari <kayamelo@gmail.com>
2020-01-07 08:24:45 +0900

    Add article and user api

--- app/routes/v1/index.js
+++ app/routes/v1/index.js
@@ -1,10 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', function(req, res){
-    res.json({
-        message: "hello world"
-    });
-});
+router.use('/article', require('./article.js'));
+router.use('/user', require('./user.js'));
 
 module.exports = router;
