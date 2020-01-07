51cdd504c9210bcfeece5b04f2c6eacb7b6feab5
takayuki.kayawari <kayamelo@gmail.com>
2020-01-07 09:22:04 +0900

    Fix post user api and add get user

--- app/routes/v1/user.js
+++ app/routes/v1/user.js
@@ -1,3 +1,5 @@
+// POST example
+// curl -X POST -H "Content-Type: application/json" -d '{"name":"kayawari", "screen_name":"taka", "bio":"fuga"}' http://localhost:3000/api/v1/user/
 var express = require('express');
 var router = express.Router();
 var UserModel = require('../../models/userModel.js');
@@ -16,6 +18,14 @@ router.post('/', function(req, res){
             res.json({message: 'success'});
         }
     });
+});
+
+router.get('/', function (req, res) {
+    UserModel
+        .find()
+        .then(function (users) {
+            res.json(users);
+        });
 });
 
 module.exports = router;
